c1ae879e0154ff8231f36bb82320934f94fc2091
Deniz <49831545+creeperkafasi@users.noreply.github.com>
2023-12-04 16:06:32 +0300

    Even more optimizations for day 4

--- src/p4.rs
+++ src/p4.rs
@@ -1,50 +1,35 @@
 use std::fs;
 
 pub fn answer() {
-    println!("First answer:  {}", first());
-    println!("Second answer: {}", second());
+    let games = input();
+    println!("First answer:  {}", first(games.clone()));
+    println!("Second answer: {}", second(games.clone()));
 }
 
-fn first() -> usize {
-    let games = input();
-
+fn first(games: Vec<Card>) -> usize {
     let mut sum = 0;
-    for Card {
-        id: _,
-        winners,
-        have,
-    } in games
-    {
+    for Card { winners, have, .. } in games {
         let f = have
             .iter()
             .filter(|n| winners.contains(n))
-            .fold(0.5_f32, |acc, _| acc * 2.0);
-        sum += f.floor() as usize;
+            .fold(1, |acc, _| acc * 2);
+        sum += f / 2;
     }
     sum
 }
 
-fn second() -> usize {
-    let games = input();
+fn second(games: Vec<Card>) -> usize {
     let mut cardcounts: Vec<usize> = vec![1; games.len()];
-
-    // for Card { id, winners, have } in &games {
-    //     cardcounts.insert(*id, 1);
-    // }
 
     let mut i = 0;
 
     loop {
         match games.get(i) {
-            Some(Card { id, winners, have }) => {
+            Some(Card { winners, have }) => {
                 let c = have.iter().filter(|n| winners.contains(n)).count();
-                for _ in 0..cardcounts[*id - 1] {
-                    for n in *id..id + c {
-                        cardcounts[n] = cardcounts[n] + 1;
-                        // games.insert(i + 1, games.iter().find(|c| c.id == n + 1).unwrap().clone());
-                    }
+                for n in 0..c {
+                    cardcounts[i + 1 + n] += cardcounts[i];
                 }
-                // dbg!(id);
             }
             None => break,
         };
@@ -53,19 +38,10 @@ fn second() -> usize {
     }
 
     cardcounts.iter().sum::<usize>()
-    // for (winner, have) in games {
-    //     have.iter()
-    //         .enumerate()
-    //         // .map(|(x, y)| (x + 1, y))
-    //         .filter(|n| winner.contains(n));
-    // }
-
-    // todo!()
 }
 
 #[derive(Clone, Debug)]
 struct Card {
-    id: usize,
     winners: Vec<usize>,
     have: Vec<usize>,
 }
@@ -80,7 +56,6 @@ fn input() -> Vec<Card> {
             let winners = s.next();
             let have = s.next();
             Card {
-                id: i + 1,
                 winners: winners
                     .unwrap()
                     .split_whitespace()
