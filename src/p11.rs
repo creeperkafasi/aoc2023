0f6bd0a0004a8576006ddffc2f0c85110dccd92b
Deniz <49831545+creeperkafasi@users.noreply.github.com>
2023-12-12 17:46:30 +0300

    Refactor Day 11

--- src/p11.rs
+++ src/p11.rs
@@ -1,11 +1,8 @@
-use std::{borrow::BorrowMut, fs};
+use std::fs;
 
 pub fn answer() -> (usize, usize) {
-    let mut f = fs::read_to_string("inputs/11.txt").unwrap();
+    let f = fs::read_to_string("inputs/11.txt").unwrap();
     let width = f.find("\n").unwrap() + 1;
-    let height = f.split("\n").count();
-
-    let mut di = 0;
 
     let mut empty_rows: Vec<usize> = vec![];
     let mut empty_cols: Vec<usize> = vec![];
@@ -16,39 +13,20 @@ pub fn answer() -> (usize, usize) {
         }
     }
 
-    // println!("{}", f);
-
-    let f0 = f.clone();
-
-    (0..(width - 1)).for_each(|x| {
+    (0..width).for_each(|x| {
         if f.lines().all(|line| line.chars().nth(x) == Some('.')) {
             empty_cols.push(x);
-            // // dbg!(x + di);
-            // f.clone()
-            //     .match_indices("\n")
-            //     .chain([(f.clone().char_indices().last().unwrap().0 + 1, "")])
-            //     .for_each(|(i, _)| {
-            //         // println!("{}\n", f.clone());
-            //         f.insert(i + x + di - width + 1, '.');
-            //         di += 1;
-            //     });
         }
     });
-
-    // dbg!(empty_cols, empty_rows);
-
-    // println!("{}", f);10276166
 
     let galaxies = f
         .lines()
         .enumerate()
         .map(|(y, line)| {
-            line.char_indices()
-                .filter_map(move |(x, c)| match c {
-                    '#' => Some(Galaxy { x: x, y: y }),
-                    _ => None,
-                })
-                .collect::<Vec<_>>()
+            line.char_indices().filter_map(move |(x, c)| match c {
+                '#' => Some(Galaxy { x: x, y: y }),
+                _ => None,
+            })
         })
         .flatten()
         .collect::<Vec<_>>();
@@ -61,57 +39,35 @@ pub fn answer() -> (usize, usize) {
 
                 let ex = empty_cols
                     .iter()
-                    .filter(|x| (**x).clamp(g1.x.min(g2.x), g1.x.max(g2.x)) == **x)
+                    .filter(|x| x.clamp(&&g1.x.min(g2.x), &&g1.x.max(g2.x)) == x)
                     .count();
 
                 let ey = empty_rows
                     .iter()
-                    .filter(|y| (**y).clamp(g1.y.min(g2.y), g1.y.max(g2.y)) == **y)
+                    .filter(|y| y.clamp(&&g1.y.min(g2.y), &&g1.y.max(g2.y)) == y)
                     .count();
 
                 (*g1, *g2, ex, ey)
-            }).collect::<Vec<_>>()
+            })
         })
         .flatten()
         .collect::<Vec<_>>();
 
-    // let info = galaxies
-    //     .iter()
-    //     .combinations(2)
-    //     .unique()
-    //     .map(|g| {
-    //         let g1 = g.first().unwrap();
-    //         let g2 = g.last().unwrap();
-
-    //         let ex = empty_cols
-    //             .iter()
-    //             .filter(|x| (**x).clamp(g1.x.min(g2.x), g1.x.max(g2.x)) == **x)
-    //             .count();
-
-    //         let ey = empty_rows
-    //             .iter()
-    //             .filter(|y| (**y).clamp(g1.y.min(g2.y), g1.y.max(g2.y)) == **y)
-    //             .count();
-
-    //         (*g1, *g2, ex, ey)
-    //     })
-    //     .collect::<Vec<_>>();
-
     (
         info.iter()
             .map(|(g1, g2, ex, ey)| {
-                g2.x.abs_diff(g1.x) + ex * 1 + g2.y.abs_diff(g1.y) + ey * 1
+                return g2.x.abs_diff(g1.x) + ex + g2.y.abs_diff(g1.y) + ey;
             })
             .sum(),
         info.iter()
             .map(|(g1, g2, ex, ey)| {
-                g2.x.abs_diff(g1.x) + ex * 999_999 + g2.y.abs_diff(g1.y) + ey * 999_999
+                return g2.x.abs_diff(g1.x) + ex * 999_999 + g2.y.abs_diff(g1.y) + ey * 999_999;
             })
             .sum(),
     )
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 struct Galaxy {
     x: usize,
     y: usize,
